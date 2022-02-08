ca21cb78ae62dee4488b6b807488340b72dc07d7
Saurabh Phawade <88394186+Saurabh3639@users.noreply.github.com>
2022-02-08 23:02:31 +0530

    Add files via upload

---
+++ blog-app/src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className="mb-20">
+            <h1 className="sm:text-4xl text-2xl font-bold mt-6 mb-6 text-gray-900">About Us</h1>
+            <p className="mx-auto leading-relaxed text-base mb-4">
+                Welcome to my Blog! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas volutpat placerat tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus convallis rutrum diam, nec pharetra mauris tincidunt ac. Fusce arcu ipsum, vestibulum a porttitor at, luctus eu quam. Pellentesque laoreet, ante vitae maximus congue, felis ipsum rutrum nisl, vel egestas quam urna ac purus. Nulla vitae molestie ex. Phasellus in fermentum libero. Curabitur leo elit, commodo nec erat consectetur, sagittis placerat sem. Cras luctus dapibus ipsum, ut feugiat erat. Praesent sollicitudin viverra felis, vitae aliquet est porta at.
+            </p>
+        </div>
+    );
+};
+
+export default About;
